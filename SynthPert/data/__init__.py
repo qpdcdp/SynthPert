1c66e835181a1e83cc795850690ff30610fbb66a
qpdcdp <lphillipspersonal@gmail.com>
2025-06-02 10:40:18 -0400

    Delete SynthPert directory

--- SynthPert/data/__init__.py
+++
@@ -1,8 +0,0 @@
-from .loaders import (
-    create_train_dataloader,
-    create_val_dataloader,
-    create_test_dataloader
-)
-
-from .dataset import DiffExpressionDataset
-from .dataset_direct_predict import GeneRegulationListDataset
--- SynthPert/model/__init__.py
+++
@@ -1,5 +0,0 @@
-from .fsdp_model import (
-    build_fsdp_model,
-    print_model_size,
-    print_fsdp_info,
-)
