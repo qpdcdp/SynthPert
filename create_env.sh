0dfe5a43f8e0ac16d282445f46ad7c85a9c60f49
qpdcdp <lphillipspersonal@gmail.com>
2025-06-07 23:20:43 -0400

    Update create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -1,10 +1,11 @@
 conda create -n synthpert python=3.11.11
 
 # Activate the new environment
+conda init
 conda activate synthpert
 
 # Install the core packages using pip
-pip install torch==2.6.0 
+pip install torch==2.7.0 torchvision --index-url https://download.pytorch.org/whl/cu121
 pip install transformers==4.51.3
 pip install accelerate==1.5.2
 pip install peft==0.15.1
@@ -16,8 +17,8 @@ pip install tqdm==4.67.1
 pip install wandb==0.19.8
 pip install datasets==3.5.0
 pip install evaluate==0.4.3
-pip install unsloth==2025.3.19
-pip install unsloth-zoo==2025.3.17
+pip install unsloth
+pip install unsloth-zoo
 
 # Additional core dependencies
 pip install safetensors==0.5.3  # Important for model loading
@@ -33,6 +34,7 @@ pip install openai==1.69.0      # If using OpenAI models
 
 # Development tools
 pip install ipykernel          # For Jupyter notebook support
+pip install cloudpickle==1.2.2
 pip install ruff==0.11.5       # For code linting
 
 pip install fsspec==2024.12.0      # File system operations
@@ -48,7 +50,7 @@ pip install filelock==3.18.0       # File locking utilities
 
 # Runtime and optimization
 pip install packaging==24.2        # Package metadata handling
-pip install typing-extensions==4.13.0  # Type hinting extensions
+pip install typing-extensions==4.14.0 # Type hints for Python 3.11
 pip install pydantic==2.11.1       # Data validation
 pip install ninja==1.11.1.4        # Build system
 
